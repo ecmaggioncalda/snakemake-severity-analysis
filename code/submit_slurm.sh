13c5f10166760be00fba6514aeba26d0f25af03d
ecmaggioncalda <emilycma@umich.edu>
2022-04-14 10:06:05 -0400

    update name of job submission and wall-time

--- code/submit_slurm.sh
+++ code/submit_slurm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH --job-name=cyokine_GWAS
+#SBATCH --job-name=severity_GWAS
 
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=1GB
-#SBATCH --time=96:00:00
+#SBATCH --time=14-00:00:00
 
 #SBATCH --output=log/hpc/slurm-%j_%x.out
 
